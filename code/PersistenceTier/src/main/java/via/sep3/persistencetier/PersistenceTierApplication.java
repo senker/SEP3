5884c0df0cf06adc33e8b483ca7a62d5bad6bfee
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-10-31 11:25:37 +0100

    adding ground work.tiers. to the repository

---
+++ code/PersistenceTier/src/main/java/via/sep3/persistencetier/PersistenceTierApplication.java
@@ -0,0 +1,13 @@
+package via.sep3.persistencetier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceTierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceTierApplication.class, args);
+	}
+
+}
---
+++ code/PersistenceTier/src/test/java/via/sep3/persistencetier/PersistenceTierApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sep3.persistencetier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceTierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
