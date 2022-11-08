755e36e49dd78cb6acb25d05e99a4118dbd63040
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-11-08 12:38:14 +0100

    feat: setting up projects and adding SellerModel, UserModel

---
+++ code/LogicTier/Domain/Models/Authentication/SellerModel.cs
@@ -0,0 +1,11 @@
+namespace Domain.Models;
+
+public class Seller
+{
+    public int Cvr { get; set; }
+    public string CompanyName { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public string Website { get; set; }
+    public float Rating { get; set; }
+}
