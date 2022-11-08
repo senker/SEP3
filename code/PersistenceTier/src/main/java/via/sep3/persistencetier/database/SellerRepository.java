043e8e12452577624a1234120611ef327ed70f0a
foxide123 <kuba.kolando.02.01@gmail.com>
2022-11-08 22:48:03 +0100

    update service methods

--- code/PersistenceTier/src/main/java/via/sep3/persistencetier/database/SellerRepository.java
+++ code/PersistenceTier/src/main/java/via/sep3/persistencetier/database/SellerRepository.java
@@ -14,4 +14,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //save
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+
 }
--- code/PersistenceTier/src/main/java/via/sep3/persistencetier/service/SellerService.java
+++ code/PersistenceTier/src/main/java/via/sep3/persistencetier/service/SellerService.java
@@ -1,19 +1,28 @@
 package via.sep3.persistencetier.service;
 
 import io.grpc.stub.StreamObserver;
+import org.springframework.beans.factory.annotation.Autowired;
 import via.sep3.persistencetier.database.Seller;
 import via.sep3.persistencetier.database.SellerRepository;
 import via.sep3.persistencetier.protobuf.CreateSellerResponse;
 import via.sep3.persistencetier.protobuf.SellerRequest;
 import via.sep3.persistencetier.protobuf.SellerResponse;
 import via.sep3.persistencetier.protobuf.SellerServiceGrpc;
-import via.sep3.persistencetier.server.DB;
 
 public class SellerService extends SellerServiceGrpc.SellerServiceImplBase {
 
+    @Autowired
+    SellerRepository sellerRepository;
     @Override
     public void createSeller(CreateSellerResponse request, StreamObserver<SellerResponse> responseObserver) {
-        Seller sellerFromDB = DB.createSeller(request);
+        Seller seller = new Seller(
+                request.getUser().getFirstName(),
+                request.getUser().getLastName(),
+                request.getUser().getAddress(),
+                request.getUser().getPhoneNumber(),
+                request.get);
+
+        sellerRepository.save(seller);
 
 
     }
@@ -21,5 +30,7 @@ public class SellerService extends SellerServiceGrpc.SellerServiceImplBase {
     @Override
     public void getSellerById(SellerRequest request, StreamObserver<SellerResponse> responseObserver) {
         super.getSellerById(request, responseObserver);
+
+        sellerRepository.getReferenceById((long) request.getId());
     }
 }
