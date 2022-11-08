1c350b213ba237eaf621fe8b28616a98c10e05b8
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-11-08 23:42:44 +0100

    feat: adding presentation tier WASM project,shared domain and httpclients

---
+++ code/PresentationTier/Domain/Models/SellerModel.cs
@@ -0,0 +1,12 @@
+namespace Domain.Models;
+
+public class SellerModel
+{
+    public UserModel User { get; set; }
+    public int Cvr { get; set; }
+    public string CompanyName { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public string Website { get; set; }
+    public float Rating { get; set; }
+}
