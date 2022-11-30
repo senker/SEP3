1f2f0be5a95d98f09d4624b8a2fd06c743a336c2
foxide123 <kuba.kolando.02.01@gmail.com>
2022-11-30 13:02:57 +0100

    cache implementation - partialy done

---
+++ code/PresentationTier/WastelessWASM/SavedRestaurant.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace WastelessWASM;
+
+public class SavedRestaurant
+{
+    private IMemoryCache _memoryCache;
+
+    public SavedRestaurant(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    public async void SetSeller(SellerModel sellerModel)
+    {
+        _memoryCache.Set("seller", sellerModel, TimeSpan.FromMinutes(1));
+    }
+
+    public Task<SellerModel> GetSeller()
+    {
+        SellerModel output;
+
+        output = _memoryCache.Get<SellerModel>("seller");
+        return Task.FromResult(output);
+    }
+}
