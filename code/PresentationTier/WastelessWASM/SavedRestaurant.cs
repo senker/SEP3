ec8e11e285eff15571f1b5e4f5bc82a422649ddc
foxide123 <kuba.kolando.02.01@gmail.com>
2022-12-01 02:29:43 +0100

    restaurant view displaying real data

--- code/PresentationTier/WastelessWASM/SavedRestaurant.cs
+++ code/PresentationTier/WastelessWASM/SavedRestaurant.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Models;
+﻿using System.Globalization;
+using Domain.Models;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace WastelessWASM;
@@ -14,11 +15,36 @@ public class SavedRestaurant
 
     public async void SetSeller(SellerModel sellerModel)
     {
-        _memoryCache.Set("seller", sellerModel, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("user_id", sellerModel.User.Id, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("first_name", sellerModel.User.FirstName, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("last_name", sellerModel.User.LastName, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("city", sellerModel.User.Address.City, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("post_code", sellerModel.User.Address.Postcode, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("street_name", sellerModel.User.Address.Streetname, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("cvr", sellerModel.Cvr, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("company_name", sellerModel.CompanyName, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("description", sellerModel.Description, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("type", sellerModel.Type, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("website", sellerModel.Website, TimeSpan.FromMinutes(1));
+        _memoryCache.Set("rating", sellerModel.Rating, TimeSpan.FromMinutes(1));
     }
 
-    public async Task<string> GetSeller()
+    public List<string> GetSeller()
     {
-        return _memoryCache.Get<SellerModel>("seller").ToString();
+        List<string> sellerList = new List<string>();
+        sellerList.Add(_memoryCache.Get<int>("user_id").ToString());
+        sellerList.Add(_memoryCache.Get<string>("first_name"));
+        sellerList.Add(_memoryCache.Get<string>("last_name"));
+        sellerList.Add(_memoryCache.Get<string>("city"));
+        sellerList.Add(_memoryCache.Get<int>("post_code").ToString());
+        sellerList.Add(_memoryCache.Get<string>("street_name"));
+            sellerList.Add(_memoryCache.Get<int>("cvr").ToString());
+            sellerList.Add(_memoryCache.Get<string>("company_name"));
+            sellerList.Add(_memoryCache.Get<string>("description"));
+            sellerList.Add(_memoryCache.Get<string>("type"));
+            sellerList.Add(_memoryCache.Get<string>("website"));
+            sellerList.Add(string.Format("{0:N2}", _memoryCache.Get<float>("rating")));
+        //return _memoryCache.Get<SellerModel>("seller").ToString();
+        return sellerList;
     }
 }
