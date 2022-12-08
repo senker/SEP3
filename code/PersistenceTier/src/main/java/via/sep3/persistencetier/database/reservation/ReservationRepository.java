f70a7b9c2ef9283c31fad64a323850a0f27cbf4e
foxide123 <kuba.kolando.02.01@gmail.com>
2022-12-08 19:50:17 +0100

    reservation service fixed

--- code/PersistenceTier/src/main/java/via/sep3/persistencetier/database/reservation/ReservationRepository.java
+++ code/PersistenceTier/src/main/java/via/sep3/persistencetier/database/reservation/ReservationRepository.java
@@ -10,6 +10,9 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query(value="SELECT * FROM reservation WHERE reservation.cvr=?1", nativeQuery = true)
     Seller findBySellerCvr(Seller seller);
 
+    @Query(value="SELECT * FROM reservation WHERE reservation.id=?1", nativeQuery = true)
+    Reservation findById(int id);
+
     @Query(value="DELETE * FROM reservation WHERE reservation.id=?1", nativeQuery = true)
     void deleteById(int id);
 }
